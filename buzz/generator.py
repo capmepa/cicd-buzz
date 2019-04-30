4118d6502cbdf7870c6cd3a1aa55c6634b3b3a00
capmepa <49653639+capmepa@users.noreply.github.com>
2019-04-30 15:04:27 +0300

    Update generator.py
    experiment 1

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import random
 
-buzz = ('HELLOnuous testing', 'HELLOnuous integration',
+buzz = ('My Name', 'HELLOnuous integration',
     'HELLOnuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
